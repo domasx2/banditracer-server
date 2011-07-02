4ef85eb7eb73014dff0b867179ef18d61d5e5ddc
Simon Oberhammer <simon@nekapuzer.at>
2011-07-02 11:46:11 +0200

    handle setting.js of server the same way as client

--- settings.js
+++
@@ -1,6 +0,0 @@
-exports.LEVEL_DIRECTORY='/home/banditracer-client-live/levels/';
-exports.GAMEJS_DIRECTORY='../gamejs/lib/';
-exports.UPDATES_PER_SECOND=60;
-exports.PLAYER_TIMEOUT=1000*60; //timeout a player in 60 seconds of inactivity
-exports.GAME_TIMEOUT=1000*60*20; //timeout a game if it doesnt end in 20 minutes.
-exports.PORT=8001;
